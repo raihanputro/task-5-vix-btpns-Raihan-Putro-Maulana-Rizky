8cc3efd3d6a84e6c353b1138fecde229c8624e57
raihanputro <raihanputromaulana477@gmail.com>
2022-12-31 19:54:18 +0700

    Adding photos endpoint

--- main.go
+++ main.go
@@ -4,7 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+type Photo struct {
+	ID int `json:"ID"`
+	Title string `json:"Title"`
+	Caption string `json:"Caption"`
+	PhotoUrl string `json:"PhotoUrl"`
+	UserID int `json:UserID`
+}
+
+type Photos []Photo
+
+func allPhotos(w http.ResponseWriter, r*http.Request) {
+	photos := Photos{
+		Photo{
+			ID: 1, 
+			Title: "Monas", 
+			Caption: "Landmark Jakarta", 
+			PhotoUrl: "https://commons.wikimedia.org/wiki/File:National_Monument_(Monas)_Jakarta.jpg", 
+			UserID: 1,
+		},
+	}
+
+	fmt.Println("Endpoint Hit: All Photos Endpoint")
+	json.NewEncoder(w).Encode(photos)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage endpoint Hit")
@@ -12,6 +38,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/photos", allPhotos)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
